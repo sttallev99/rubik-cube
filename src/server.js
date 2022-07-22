af575fc7a2504b1418e31e2e259fb4fe69894029
Георги Сталев <sttallev99@gmail.com>
2022-07-22 15:27:17 +0300

    add Express-handlebars

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
+app.engine('hbs', engine({ extname: 'hbs', defaultLayout: false }));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.all('/', (req, res) => {
-    res.write('It\'s working!');
-    res.end();
+    res.render('index');
 });
 
 app.listen(5000, () => console.log('Application is running on http://localhost:5000'))
