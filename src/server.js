f8f772f6c36963869139dd6ada28607da1c55d29
Георги Сталев <sttallev99@gmail.com>
2022-07-22 15:12:32 +0300

    Start express app

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-console.log('It work')
+const express = require('express');
+
+const app = express();
+
+app.all('/', (req, res) => {
+    res.write('It\'s working!');
+    res.end();
+});
+
+app.listen(5000, () => console.log('Application is running on http://localhost:5000'))
