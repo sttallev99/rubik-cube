1dd6c07626cc67b1bed2c993a9b83a55bc1a4dca
Георги Сталев <sttallev99@gmail.com>
2022-07-25 16:10:37 +0300

    Add search by difficulty

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,3 +1,4 @@
+const { text } = require('express');
 const Cube = require('../models/Cube');
 
 const cubeDb = [
@@ -13,11 +14,28 @@ const getAll = () => Cube.cubes
 
 const getOneCube = (id) => Cube.cubes.find(x => x.id === id);
 
-const search = (text, from, to) => Cube.cubes.filter(x => x.name.toLowerCase().includes(text.toLowerCase()));
+const search = (text, from, to) => {
+    let result = Cube.cubes;
+    console.log(result)
+
+    if(text) {
+        result = result.filter(x => x.name.toLowerCase().includes(text.toLowerCase()));
+    }
+
+    if(from) {
+        result = result.filter(x => x.difficulty >= from)
+    }
+
+    if(to) {
+        result = result.filter(x => x.difficulty <= to);
+    }
+    
+    return result;
+    
+}
 
 const create = (name, description, imageUrl, difficulty) => {
     let cube = new Cube(name, description, imageUrl, difficulty);
-
     Cube.add(cube);
 }
 
