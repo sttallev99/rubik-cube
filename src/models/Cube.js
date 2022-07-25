cc4924bc20b07258152f6706b927c3c80b08ac41
Георги Сталев <sttallev99@gmail.com>
2022-07-25 12:00:38 +0300

    Browse dynamic cubes

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,9 +1,37 @@
+const uniqid = require('uniqid');
+
 class Cube {
+    static #cubes = [
+        {
+            id: 'asdofmadskdls',
+            name: 'Mirror Cube',
+            description: 'Strange Cube',
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Mirror_Cube_solved.png/220px-Mirror_Cube_solved.png',
+            difficulty: '4'
+        },
+        {
+            id: '1lvc0id4l60im26i',
+            name: 'Ice cube',
+            description: 'Ice Cube description',
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Ice-Cube_2014-01-09-Chicago-photoby-Adam-Bielawski.jpg/250px-Ice-Cube_2014-01-09-Chicago-photoby-Adam-Bielawski.jpg',
+            difficulty: '1'
+          }
+    ];
+
     constructor(name, description, imageUrl, difficulty) {
+        this.id = uniqid();
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
         this.difficulty = difficulty;
+    }
+
+    static get cubes() {
+        return Cube.#cubes.slice();
+    }
+
+    static add(cube) {
+        Cube.#cubes.push(cube);
     }
 }
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -9,12 +9,12 @@ const cubeDb = [
     }
 ];
 
-const getAll = () => cubeDb.slice();
+const getAll = () => Cube.cubes
 
 const create = (name, description, imageUrl, difficulty) => {
     let cube = new Cube(name, description, imageUrl, difficulty);
 
-    cubeDb.push(cube);
+    Cube.add(cube);
 }
 
 const cubeService = {
