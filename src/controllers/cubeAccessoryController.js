a58e8c09860497e8d167ab403c024603b9851973
Георги Сталев <sttallev99@gmail.com>
2022-07-29 14:02:27 +0300

    Show accessory select on accessory attach page

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -1,11 +1,14 @@
 const router = require('express').Router({ mergeParams: true });
 
 const cubeService = require('../services/cubeService');
+const accessoryService = require('../services/accessoryService');
 
 router.get('/add', async(req, res) => {
 
     const cube = await cubeService.getOneCube(req.params.id);
-    res.render('cube/accessory/add', cube);
+    const accessories = await accessoryService.getAccessories();
+    console.log(accessories)
+    res.render('cube/accessory/add', {cube, accessories});
 });
 
 module.exports = router;
--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -1,11 +1,16 @@
 const Accessory = require('../models/Accessory');
 
+async function getAccessories() {
+    return await Accessory.find({}).lean();
+}
+
 async function create(name, imageUrl, description) {
-    return Accessory.create({name, imageUrl, description});
+    return await Accessory.create({name, imageUrl, description});
 }
 
 const accessoryService = {
-    create
+    create,
+    getAccessories
 }
 
 module.exports = accessoryService;
