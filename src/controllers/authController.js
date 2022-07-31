f57089674344fae058c14a90c4f12652428910b9
Георги Сталев <sttallev99@gmail.com>
2022-07-31 23:25:36 +0300

    Render register page with get and post methods

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,9 +4,19 @@ router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+router.get('/register', (req, res) => {
+    res.render('auth/register')
+});
+
 router.post('/login', (req, res) => {
     console.log(req.body);
 
     res.redirect('/login');
+});
+
+router.post('/register', (req, res) => {
+    console.log(req.body);
+
+    res.redirect('/register');
 });
 module.exports = router;
