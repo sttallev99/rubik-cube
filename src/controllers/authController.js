dca025f01699bb666bfac54e71a03a4c11955ca1
Георги Сталев <sttallev99@gmail.com>
2022-07-31 23:11:54 +0300

    add Login post action

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,4 +3,10 @@ const router = require('express').Router();
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
+
+router.post('/login', (req, res) => {
+    console.log(req.body);
+
+    res.redirect('/login');
+});
 module.exports = router;
