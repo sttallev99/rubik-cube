9572366491336796d3037c02f13d52de24024619
Георги Сталев <sttallev99@gmail.com>
2022-07-31 22:43:17 +0300

    add status 404 for missing pages and route for  authController

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,16 @@ const express = require('express');
 const cubeController = require('./controllers/cubeControllers.js');
 const homeController = require('./controllers/homeController.js');
 const accesoryController = require('./controllers/accessoryController');
+const authController = require('./controllers/authController');
 
 const router = express.Router();
 
 router.use(homeController);
 router.use('/cube', cubeController);
 router.use('/accessory', accesoryController);
+router.use(authController);
 router.use('*', (req, res) => {
-    res.render('404');
+    res.status(404).render('404');
 })
 
 module.exports = router;
