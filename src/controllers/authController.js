97d15962fcede10895035bf5e8f14fac4902d201
Георги Сталев <sttallev99@gmail.com>
2022-08-01 15:04:27 +0300

    Add register user with hashed password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const authService = require('../services/authService');
+
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
@@ -14,9 +16,10 @@ router.post('/login', (req, res) => {
     res.redirect('/login');
 });
 
-router.post('/register', (req, res) => {
-    console.log(req.body);
+router.post('/register', async(req, res) => {
+    let {username, password, repeatPassword} = req.body;
+    await authService.register(username, password, repeatPassword);
 
-    res.redirect('/register');
+    res.redirect('/login')
 });
 module.exports = router;
